ec77468f1fa3be0e3faea267ad2e58d27338c223
24IMCA33 <24IMCA33@mca.com>
2026-08-19 13:07:53 +0530

    Update

--- day1.js
+++ day1.js
@@ -1,10 +1,15 @@
-let age = Number(prompt("Enter Your Age: "));
-console.log(age);
+// let age = Number(prompt("Enter Your Age: "));
+// console.log(age);
 
-if(age>=18){
-    console.log("elder");
-}
-else{
-    console.log("minor");
+// if(age>=18){
+//     console.log("elder");
+// }
+// else{
+//     console.log("minor");
+// }
+
+
+for(let i=0;i<=5;i++){
+    console.log(i);
 }
 // this is lab program
